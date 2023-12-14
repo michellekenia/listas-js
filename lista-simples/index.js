c4b0de070831914e630d79c6000884dc7be9521c
michellekenia <keniamsoaress@gmail.com>
2023-12-13 22:07:23 -0300

    atualizar-listas

--- lista-simples/index.js
+++ lista-simples/index.js
@@ -6,10 +6,25 @@ const produtos = [
     { id: 5, nome:"chocolate", valor: 15.00, categoria: "alimento"}
 ]; 
 
-// função callback produto 
-// const ids = produtos.map(produto => produto.id); 
+//.map
+
+const ids = produtos.map(produto => produto.id);
+console.log(ids);
 
 const nomes = produtos.map(produtos => produtos.nome);
-
 console.log(nomes); 
 
+//.filter
+const alimentos = produtos.filter(produtos => produtos.categoria === 'alimento'); 
+console.log(alimentos.map(alimentos => alimentos.nome));
+
+//.reduce
+
+const numeros = [1, 2, 3, 4]; 
+
+const total = numeros.reduce((acumulado, numero) => acumulado + numero); 
+
+console.log(total); 
+
+const totali = produtos.reduce((acc, produto) => acc + produto.valor, 0); 
+console.log(totali); 
